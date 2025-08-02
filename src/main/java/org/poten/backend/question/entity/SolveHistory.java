17df56b09173d092274b185e33a826b0e3388cef
fnzl54 <rnjscksdud1@naver.com>
2025-08-02 18:30:46 +0900

    FEAT: (NONE) domain별 entity 추가

---
+++ src/main/java/org/poten/backend/question/entity/SolveHistory.java
@@ -0,0 +1,30 @@
+package org.poten.backend.question.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.poten.backend.global.entity.BaseEntity;
+import org.poten.backend.user.entity.User;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "solve_history")
+public class SolveHistory extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(nullable = false)
+    private Boolean isTrue;
+}
---
+++ src/main/java/org/poten/backend/user/entity/User.java
@@ -0,0 +1,39 @@
+package org.poten.backend.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.poten.backend.global.entity.BaseEntity;
+import org.poten.backend.question.entity.Question;
+import org.poten.backend.question.entity.SolveHistory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SolveHistory> solveHistories = new ArrayList<>();
+}
