48c79ed7f20609fcfcc8a66a3c7136e36ad340cb
fnzl54 <rnjscksdud1@naver.com>
2025-08-03 17:41:17 +0900

    feat: (#7) 유저의 날짜별 제작한 문제를 조회

--- src/main/java/org/poten/backend/question/repository/QuestionRepository.java
+++ src/main/java/org/poten/backend/question/repository/QuestionRepository.java
@@ -1,7 +1,11 @@
 package org.poten.backend.question.repository;
 
 import org.poten.backend.question.entity.Question;
+import org.poten.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findByUser(User user);
 }
