89184d646740b869b4dd17084df50e1677a85435
fnzl54 <rnjscksdud1@naver.com>
2025-08-03 17:41:17 +0900

    feat: (#7) 유저의 날짜별 제작한 문제를 조회

---
+++ src/main/java/org/poten/backend/question/dto/response/QuestionListResponse.java
@@ -0,0 +1,18 @@
+package org.poten.backend.question.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionListResponse {
+    private LocalDate date;
+    private List<QuestionResponse> questions;
+}
