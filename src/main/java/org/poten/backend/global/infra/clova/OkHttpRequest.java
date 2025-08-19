727e1eeac4bf45e30f0d0b00c46545bb75fd6d24
yeonchaepark <check4732@naver.com>
2025-08-19 16:02:33 +0900

    fix(#49): 응답 시간 조절

--- src/main/java/org/poten/backend/global/infra/clova/OkHttpRequest.java
+++ src/main/java/org/poten/backend/global/infra/clova/OkHttpRequest.java
@@ -17,7 +17,7 @@ public abstract class OkHttpRequest {
     private static final OkHttpClient client = new OkHttpClient.Builder()
         .connectTimeout(60, java.util.concurrent.TimeUnit.SECONDS)
         .writeTimeout(120, java.util.concurrent.TimeUnit.SECONDS)
-        .readTimeout(300, java.util.concurrent.TimeUnit.SECONDS) // 응답 대기 시간
+        .readTimeout(600, java.util.concurrent.TimeUnit.SECONDS) // 응답 대기 시간
         .build();
 
     public static Response createRequest(Request request) {
